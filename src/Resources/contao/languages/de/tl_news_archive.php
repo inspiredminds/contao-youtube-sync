75c77801b2b3f9573d08bd86d15d701431a7ca19
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2020-06-09 10:46:06 +0100

    add German translation

---
+++ src/Resources/contao/languages/de/tl_news_archive.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Contao YouTube Sync extension.
+ *
+ * (c) inspiredminds
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_sync_legend'] = 'YouTube Synchronisation';
+$GLOBALS['TL_LANG']['tl_news_archive']['enable_youtube_sync'] = ['YouTube Synchronisation aktivieren', 'Aktiviert die YouTube Synchronisation für dieses Archiv.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_playlist_id'] = ['YouTube Playlist ID', 'Von dieser YouTube Playlist werden die Videos als Nachrichten importiert.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_sync_author'] = ['Autor', 'Dieser Autor wird den importierten Nachrichten zugewiesen.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_sync_publish'] = ['Neue Einträge veröffentlichen', 'Neue Einträge werden nach der Synchronisierung automatisch veröffentlicht.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_sync_update'] = ['Bestehende Einträge aktualisieren', 'Aktualisiert bestehende Einträge automatisch mit eventuellen neuen Daten.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_sync_dir'] = ['Zielverzeichnis', 'Zielverzeichnis für herunter geladene Dateien.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['youtube_sync_trigger'] = 'YouTube sync';

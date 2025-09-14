d3af86c45957b5d599d2bf41bc5df5b6b9951145
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-09-14 14:26:10 +0200

    Fix back end controller

--- src/Controller/SyncController.php
+++ src/Controller/SyncController.php
@@ -8,16 +8,19 @@ declare(strict_types=1);
 
 namespace InspiredMinds\ContaoYouTubeSync\Controller;
 
+use Contao\CoreBundle\Controller\AbstractBackendController;
 use Contao\NewsModel;
 use InspiredMinds\ContaoYouTubeSync\Sync\NewsYouTubeSync;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
-#[Route('/contao/youtubesync', name: self::class, defaults: ['_scope' => 'backend', '_token_check' => false])]
-class SyncController
+#[AsController]
+#[Route('%contao.backend.route_prefix%/youtubesync', name: self::class, defaults: ['_scope' => 'backend', '_token_check' => false])]
+class SyncController extends AbstractBackendController
 {
     public function __construct(
         private readonly NewsYouTubeSync $newsYouTubeSync,
@@ -33,15 +36,14 @@ class SyncController
         ($this->newsYouTubeSync)();
         $count = (int) NewsModel::countAll() - $count;
 
-        return new Response(
-            $this->twig->render('@ContaoYouTubeSync/sync.html.twig', [
+        return
+            $this->render('@ContaoYouTubeSync/sync.html.twig', [
                 'section_headline' => $this->translator->trans('contao_youtube_sync.backend.section_headline'),
                 'submit_label' => $this->translator->trans('contao_youtube_sync.backend.submit_label'),
                 'back' => $this->translator->trans('MSC.goBack', [], 'contao_default'),
                 'form_action' => $this->urlGenerator->generate(self::class),
                 'count' => $count,
                 'count_label' => $this->translator->trans('contao_youtube_sync.backend.count_label', ['%count%' => $count]),
-            ]),
-        );
+            ]);
     }
 }
